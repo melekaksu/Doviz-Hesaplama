a7063a98ec4b0c1259e9727a209a1032121ffa2f
Melek AKSU <127103424+melekaksu@users.noreply.github.com>
2024-05-26 18:34:44 +0300

    Döviz Uygulaması
    Reactjs kullanıldı

---
+++ 8.Doviz-Uygulamasi/src/components/Currency.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import '../css/currency.css'
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import axios from 'axios'
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest"
+let API_KEY = "lusVgReTFOj30VUYhVAH8hsdRvyCsVSQ8uuEbNns"
+
+function Currency() {
+
+  const[amount, setAmount] = useState(0)
+  const[fromCurrency, setFromCurrency] = useState('USD')
+  const[toCurrency, setToCurrency] = useState('TRY')
+  const[result, setResult] = useState(0)
+
+  const exchange = async () => {
+    // console.log(amount)
+    // console.log(fromCurrency)
+    // console.log(toCurrency)
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`)
+    const result = (response.data.data[toCurrency] * amount).toFixed(2)
+    setResult(result)
+  }
+
+  return (
+    <div className='currency-div'>
+      <div style={{fontFamily: 'arial', backgroundColor: 'black', color: 'white', width: '100%', textAlign: 'center'}}>
+        <h3>DÖVİZ KURU UYGULAMASI</h3>
+      </div>
+
+      <div style={{marginTop: '25px'}}>
+        <input 
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        type="number" className='amount' />
+
+        <select onChange={(e) => setFromCurrency(e.target.value)} className='from-currency-option'>
+          <option>USD</option>
+          <option>EUR</option>
+          <option>TRY</option>
+        </select>
+
+        <FaArrowAltCircleRight style={{fontSize: '25px', marginRight:'10px', marginTop: '10px' }}/>
+
+        <select onChange={(e) => setToCurrency(e.target.value)} className='to-currency-option'>
+          <option>TRY</option>
+          <option>USD</option>
+          <option>EUR</option>
+        </select>
+
+        <input value={result} type="number" className='result' />
+      </div>
+
+      <div>
+       <button 
+       onClick={exchange}
+       className='exchange-button'>Çevir</button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Currency
